a0e015baa80a9c67b6896bc0c7f70fe6f1ff345b
artem <Ivanovav@answer-42.ru>
2023-12-04 17:14:34 +0300

    Написали BeanFactoryPostProcessor, работает до того, как создается бин и может подкрутить BeanDefinition

---
+++ src/main/java/com/temimo/springthereaper/quoters/T1000.java
@@ -0,0 +1,8 @@
+package com.temimo.springthereaper.quoters;
+
+public class T1000 extends TerminatorQuoter {
+    @Override
+    public void sayQuote() {
+        System.out.println("Я ЖИДКИЙ");
+    }
+}
